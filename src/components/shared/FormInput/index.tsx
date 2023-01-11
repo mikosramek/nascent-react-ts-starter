5867389d26d417fa89000b318a8f3eb6d9f437be
mikosramek <miko@mikosramek.ca>
2023-01-11 10:00:00 -0500

    cleanup: unneeded base code + custom input

---
+++ src/components/shared/FormInput/index.tsx
@@ -0,0 +1,39 @@
+import { HTMLInputTypeAttribute } from "react";
+import * as Styled from "./FormInput.styled";
+
+type Props = {
+  inputName: string;
+  label: string;
+  type?: HTMLInputTypeAttribute;
+  value: string;
+  onChange: (name: any, input: string) => void;
+  errorMessage?: string;
+};
+
+export const FormInput = ({
+  inputName,
+  label,
+  type = "text",
+  value,
+  onChange,
+  errorMessage,
+}: Props) => {
+  return (
+    <>
+      <Styled.Label htmlFor={inputName}>{label}:</Styled.Label>
+      {!!errorMessage && (
+        <Styled.ErrorLabel htmlFor={inputName}>
+          {errorMessage}
+        </Styled.ErrorLabel>
+      )}
+      <Styled.InputBase
+        type={type}
+        id={inputName}
+        name={inputName}
+        value={value}
+        onChange={(e) => onChange(inputName, e.target.value)}
+        aria-invalid={!!errorMessage}
+      />
+    </>
+  );
+};
---
+++ src/hooks/form/useInput.ts
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import cloneDeep from "lodash.clonedeep";
+
+export type BaseInputs = Record<
+  string,
+  {
+    val: string;
+    label: string;
+    validate: (val: string) => boolean;
+    errorString: string;
+    error?: string;
+    specialType?: "textarea";
+  }
+>;
+
+type Props = {
+  baseInputs: BaseInputs;
+};
+
+export type handleInputChangeProps = (inputName: string, value: string) => void;
+
+export const useInput = ({ baseInputs }: Props) => {
+  const [inputs, setInputs] = useState(cloneDeep(baseInputs));
+
+  const handleInputChange = (
+    inputName: keyof typeof baseInputs,
+    value: string
+  ) => {
+    setInputs({
+      ...inputs,
+      [inputName]: {
+        ...inputs[inputName],
+        val: value,
+      },
+    });
+  };
+
+  const validateInputs = () => {
+    const inputCopy = cloneDeep(inputs);
+    let isFormValid = true;
+    for (const inputObject of Object.values(inputCopy)) {
+      const isInputValid = inputObject.validate(inputObject.val);
+      inputObject.error = !isInputValid ? inputObject.errorString : undefined;
+      if (!isInputValid) {
+        isFormValid = false;
+      }
+    }
+    setInputs(inputCopy);
+    return isFormValid;
+  };
+
+  return { inputs, handleInputChange, validateInputs };
+};
